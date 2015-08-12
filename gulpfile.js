8b3738fc04b4897f8245b1bb23f32dbebc412c47
v-yiyao <v-yiyao@microsoft.com>
2015-08-12 14:51:32 +0800

    add deploy

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,39 @@ var gulp = require('gulp');
 var wiredep = require('wiredep').stream;
 var plugins = require('gulp-load-plugins')();
 
-gulp.task('wiredep', function() {
+gulp.task('wiredep', function () {
   gulp.src('src/index.html')
-  .pipe(wiredep())
-  .pipe(gulp.dest('src'));
+    .pipe(wiredep())
+    .pipe(gulp.dest('src'));
 });
 
-gulp.task('usemin', function() {
+gulp.task('usemin', function () {
   return gulp.src('src/index.html')
-  .pipe(plugins.usemin({
-    // css: [plugins.minifyCss(), 'concat'],
-    html: [plugins.minifyHtml({empty: true})],
-    js: [plugins.uglify(), plugins.rev()],
-    appjs: [plugins.uglify()],
-  }))
-  .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('serve', ['build'], function() {
-  gulp.src('dist')
-  .pipe(plugins.webserver({
-    host: 'localhost',
-    port: 8080,
-    directoryListing: false,
-    livereload: true
-  }));
+    .pipe(plugins.usemin({
+      // css: [plugins.minifyCss(), 'concat'],
+      html: [plugins.minifyHtml({ empty: true })],
+      js: [plugins.uglify(), plugins.rev()],
+      appjs: [plugins.uglify()],
+    }))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('build', ['wiredep', 'usemin']);
 
-gulp.task('default', ['build'], function() {
+gulp.task('deploy', ['build'], function () {
+  gulp.src('./dist/**/*')
+  .pipe(plugins.ghPages());
+});
+
+gulp.task('serve', ['build'], function () {
+  gulp.src('dist')
+    .pipe(plugins.webserver({
+      host: 'localhost',
+      port: 8080,
+      directoryListing: false,
+      livereload: true
+    }));
+});
+
+gulp.task('default', ['build'], function () {
 });
